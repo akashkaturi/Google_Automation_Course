bc184795d8381e8247992e1e1493e968bd1110fe
akashkaturi <64057380+akashkaturi@users.noreply.github.com>
2021-11-02 13:33:01 +0530

    change

--- basics/age.py
+++ basics/age.py
@@ -1,12 +1,19 @@
 import mysql.connector
-while True:
-    name = input("Enter you name: ")
-    age = input("Enter age: ")
-    if age.isalnum():
-        break
-    print("Enter correct value")
-while True:
-    password = input("Select a new password: (only alphabets and numbers)")
-    if password.isalnum():
-        break
-    print("Password can only contain letters and numbers")
+mydb=mysql.connector.connect(host="localhost",user="root",passwd="8341415122")
+mycursor=mydb.cursor()
+for i in mycursor:
+    print(i)
+# while True:
+#     name = input("Enter
+# you name: ")
+#     age = input("Enter age: ")
+#     if age.isalnum():
+#         break
+#     print("Enter correct value")
+# while True:
+#     password = input("Select a new password: (only alphabets and numbers)")
+#     if password.isalnum():
+#         break
+#     print("Password can only contain letters and numbers")
+
+
